1eb96a320e061f92dcd8edeae42bc35ad124dfd6
CREDO23 <thierrybakera12@gmail.com>
2022-11-21 16:16:59 +0200

    feat : new chat event when a new chat has been created

--- src/socket.ts
+++ src/socket.ts
@@ -6,23 +6,20 @@ export default class SocketInit {
 
     private static instance : SocketInit
 
-    socket : Socket
-
     io : Server
 
     sessions : Map<string,string> = new Map()
+    
 
     constructor(io : Server) {
         this.io = io 
         this.io.on('connection', async (socket : Socket) => {
-            this.socket = socket
+         
 
           const rooms = await chat.find({ users: { $in: [`${socket.handshake.auth.userId}`] } }).select({
             name : 1,
             _id : 0
           })
-
-          console.log(socket)
 
           rooms.forEach(room => socket.join(room.name))
             console.log(io.engine.clientsCount);
@@ -40,7 +37,7 @@ export default class SocketInit {
     }
 
     public  newChat(chatName : string , chat){
-        this.socket.broadcast.to(chatName).emit('newChat' , chat)
+        this.io.in(chatName).emit('newChat' , chat)
     }
 
 }
