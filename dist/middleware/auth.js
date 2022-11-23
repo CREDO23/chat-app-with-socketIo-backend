482487548f64f28882960f800de7de6d4bf13002
CREDO23 <thierrybakera12@gmail.com>
2022-11-23 10:34:49 +0200

    refact : deploy scripts

--- dist/middleware/auth.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=auth.js.map
--- dist/routes/message.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require("express");
-const messageControllers = require("../controllers/message");
-const router = express.Router();
-router.post('/', messageControllers.createMessage);
-exports.default = router;
-//# sourceMappingURL=message.js.map
